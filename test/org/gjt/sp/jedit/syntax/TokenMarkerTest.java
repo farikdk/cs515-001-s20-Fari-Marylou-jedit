5e0876564af0e937e8eede45420e50cb92309fa8
mlnash2 <team.marylou@gmail.com>
2020-04-05 12:51:04 -0600

    New test & configurations to enable testing

---
+++ test/org/gjt/sp/jedit/syntax/TokenMarkerTest.java
@@ -0,0 +1,47 @@
+/*
+ * jEdit - Programmer's Text Editor
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright © 2020 jEdit contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit.syntax;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TokenMarkerTest {
+
+
+    @Before
+    public void setUp() {
+        //TokenMarker tokenMark = new TokenMarker();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void emptyTest() {
+        assertEquals(10, 10);
+    }
+
+}
